0c0a1f14789a516ca2ff99f4b07c199e18cf93c6
begev <151647367+begev@users.noreply.github.com>
2023-12-10 16:21:52 +0200

    Update wordsTest.js

--- test/wordsTest.js
+++ test/wordsTest.js
@@ -13,16 +13,16 @@ describe('words', () => {
         expect(result).to.deep.equal(['apple', 'banana', 'cherry']);
     });
 
-    it('should split a string into words, considering punctuation as part of the word', () => {
+    it('should split a string into words, not considering punctuation as part of the word', () => {
         const string = 'apple, banana. cherry!';
         const result = words(string);
-        expect(result).to.deep.equal(['apple,', 'banana.', 'cherry!']);
+        expect(result).to.deep.equal(['apple', 'banana', 'cherry']);
     });
 
     it('should split a string into words, using a custom pattern', () => {
-        const string = 'apple, banana. cherry!';
-        const pattern = /[^, .]+/g;
+        const string = 'fred, barney, & pebbles';
+        const pattern = /\b\w+\b/g;
         const result = words(string, pattern);
-        expect(result).to.deep.equal(['apple', 'banana', 'cherry']);
+        expect(result).to.deep.equal(['fred', 'barney', 'pebbles']);
     });
 });
